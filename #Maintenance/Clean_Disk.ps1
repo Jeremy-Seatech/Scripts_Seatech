d6049fb261a25068324cca620455365b23bb7cf8
Jeremy <146735790+Jeremy-Seatech@users.noreply.github.com>
2025-10-11 00:09:28 +0200

    Add files via upload

---
+++ #Maintenance/Clean_Disk.ps1
@@ -0,0 +1,9 @@
+# Exécuter le nettoyage de disque silencieusement
+$cleanmgr = "cleanmgr.exe"
+$args = "/sagerun:1"
+
+# Lancer le processus de nettoyage
+Start-Process -FilePath $cleanmgr -ArgumentList $args -WindowStyle Hidden -Wait
+
+# Afficher un message de confirmation
+Write-Host "Le nettoyage de disque a été effectué avec succès."
