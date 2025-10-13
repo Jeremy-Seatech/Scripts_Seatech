c1e089704252eef410e9b5b54308502bc8257eae
Jeremy <146735790+Jeremy-Seatech@users.noreply.github.com>
2025-10-13 23:52:05 +0200

    Update Clean_Disk.ps1

--- Powershell/Maintenance/Clean_Disk.ps1
+++ Powershell/Maintenance/Clean_Disk.ps1
@@ -3,7 +3,7 @@ $cleanmgr = "cleanmgr.exe"
 $args = "/sagerun:1"
 
 # Lancer le processus de nettoyage
-Start-Process -FilePath $cleanmgr -ArgumentList $args -Wait
+Start-Process -FilePath $cleanmgr -ArgumentList $args -WindowStyle Hidden -Wait
 
 # Afficher un message de confirmation
 Write-Host "Le nettoyage de disque a été effectué avec succès."
